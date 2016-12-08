74044b334139b773fb5840cd76d0b3315cb42d59
Rivaler <suikofire@gmail.com>
2016-12-08 15:51:27 +0100

    Adds changeUsername and changeColors functions to user

--- models/user.js
+++ models/user.js
@@ -8,6 +8,38 @@ exports = module.exports = {
             messages : [],
             color    : '#000000'
         }
+    },
+
+    changeUsername : function (id, newUsername, broadcast = false) {
+
+        var {users} = require('../services/database');
+
+        users.findOne({_id : id}, (err, user) => {
+
+            if(!err && user){
+                var oldUsername = user.username;
+
+                users.update({_id : id}, { $set : {username : newUsername}}, (err, numReplaced) => {
+
+                    if(broadcast && !err && (numReplaced > 0)){
+
+                        var {io} = require('../services/sockets');
+                        io.emit('username change', `User '${oldUsername}' changed name to '${newUsername}'`);
+
+                    }
+                });
+            }
+
+        });
+
+    },
+
+    changeColor : function (id, newColor) {
+
+        var {users} = require('../services/database');
+
+        users.update({_id : id}, { $set : {color : newColor}});
+
     }
 
 };
